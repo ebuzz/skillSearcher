199ce53013dd93940947b28aecd910897853059a
erick <erick>
2015-10-16 13:22:21 -0700

    Correcion de metodos en DataFixture

--- src/AppBundle/DataFixtures/ORM/Users.php
+++ src/AppBundle/DataFixtures/ORM/Users.php
@@ -13,11 +13,11 @@ class Users implements FixtureInterface
 	public function load(ObjectManager $manager)
 	{
 		$users = array(
-		array('name' => 'Juan', 'lastName' => 'Aguayo', 'surName' => 'Sanchez', 'email' => 'jvalenzuela@arkusnexus.com', 'Password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
-		array('name' => 'Mariana', 'lastName' => 'Valenzuela', 'surName' => 'Marquez', 'email' => 'jvalenzuela@arkusnexus.com', 'Password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
-		array('name' => 'Alberto', 'lastName' => 'Gonzalez', 'surName' => 'Lara', 'email' => 'jvalenzuela@arkusnexus.com', 'Password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
-		array('name' => 'Araceli', 'lastName' => 'Martinez', 'surName' => 'Rojas', 'email' => 'jvalenzuela@arkusnexus.com', 'Password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
-		array('name' => 'Cristina', 'lastName' => 'Lara', 'surName' => 'Sanchez', 'email' => 'jvalenzuela@arkusnexus.com', 'Password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
+		array('name' => 'Juan', 'lastName' => 'Aguayo', 'surName' => 'Sanchez', 'email' => 'jvalenzuela@arkusnexus.com', 'password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
+		array('name' => 'Mariana', 'lastName' => 'Valenzuela', 'surName' => 'Marquez', 'email' => 'jvalenzuela@arkusnexus.com', 'password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
+		array('name' => 'Alberto', 'lastName' => 'Gonzalez', 'surName' => 'Lara', 'email' => 'jvalenzuela@arkusnexus.com', 'password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
+		array('name' => 'Araceli', 'lastName' => 'Martinez', 'surName' => 'Rojas', 'email' => 'jvalenzuela@arkusnexus.com', 'password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
+		array('name' => 'Cristina', 'lastName' => 'Lara', 'surName' => 'Sanchez', 'email' => 'jvalenzuela@arkusnexus.com', 'password' => '1234', 'position' => 'Sr Software Developer', 'admissionDate' => '12-12-12', 'roleId' => 1, 'positionId' => 1 ),
 		// ...
 		);
 		foreach ($users as $user) 
@@ -26,9 +26,9 @@ class Users implements FixtureInterface
 			$entity->setName($user['name']);
 			$entity->setLastName($user['lastName']);
 			$entity->setSurName($user['surName']);
-			$entity->setMail($user['email']);
+			$entity->setEmail($user['email']);
 			$entity->setPassword($user['password']);
-			$entity->setPosition($user['position']);
+			// $entity->setPosition($user['position']);
 			$entity->setAdmissionDate(new \DateTime("now"));
 			$entity->setImage('imagen.jotapege');
 			// We need to find a way to insert the Object itself with some Id, maybe a query?
