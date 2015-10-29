f2af069daeb3bd1655b9abaf35abb9d470bebe52
erick <erick>
2015-10-28 19:17:49 -0700

    Cambios en frontend de Busqueda de usuarios

--- web/js/user-search.js
+++ web/js/user-search.js
@@ -4,3 +4,27 @@ $(document).ready(function(){
 		$('#search-form').attr('action', select.val());
 	});
 });
+
+var openCharm = false;
+function showCharm(id,idUser){
+    skillHidder();
+    var ide = "#"+id+"-charm";
+    //console.log(ide);
+    var  charm = $("#"+id+"-charm").data("charm");
+    if (charm.element.data("opened") === true) {
+        charm.close();
+    } else {
+        charm.open();
+        showSkillsFromUser(idUser);
+    }
+ }
+ 
+function showSkillsFromUser(idUser){
+    var element = "#user-"+idUser;
+    $(element).show();
+}
+
+function skillHidder(){
+    $(".user-skills").hide();
+}
+
