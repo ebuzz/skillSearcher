5c6fec77307b20d522cec8bb4d60b749cf01d9a2
rafa25111 <1b.rafael.rios@gmail.com>
2015-11-27 12:59:17 -0800

    cambio completo de olvidar contraseña, dashboard y selector que indica en que pagina estas posicionado

--- web/js/login/login.js
+++ web/js/login/login.js
@@ -1,45 +1,60 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
+$(function() {
 
-	  if (e.type === 'keypress') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
+  $('.form').find('input, textarea').on('keyup blur focus', function (e) {
+    
+    var $this = $(this),
+        label = $this.prev('label');
+
+      if (e.type === 'keypress') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
+          label.removeClass('active highlight'); 
         } else {
-          label.addClass('active highlight');
+          label.removeClass('highlight');   
+        }   
+      } else if (e.type === 'focus') {
+        
+        if( $this.val() === '' ) {
+          label.addClass('active')
+          label.removeClass('highlight'); 
+        } 
+        else if( $this.val() !== '' ) {
+          label.addClass('active')
+          label.addClass('highlight');
         }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-        label.addClass('active')
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-        label.addClass('active')
-		    label.addClass('highlight');
-			}
+      }
+
+  });
+
+  $('.tab a').on('click', function (e) {
+    
+    e.preventDefault();
+    
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+    
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+    
+    $(target).fadeIn(600);
+    
+  });
+
+  var showPass = $('.form-control-feedback');
+  showPass.click(function(){
+    var clicks = $(this).data('clicks');
+    console.log(clicks);
+     if (clicks) {
+      $(this).prev('input').prop('type','text');
+    } else{
+      $(this).prev('input').prop('type','password');
     }
-
-});
-
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
+    $(this).data("clicks", !clicks);
+  });
 });
--- web/js/user/register.js
+++ web/js/user/register.js
@@ -22,12 +22,6 @@ $(document).ready(function () {
         }
     }
 
-    $('.btn-file :file').on('change', function (event) {
-        var input = $(this).parents('.input-group').find(':text');
-        var output = document.getElementById('output');
-        output.src = URL.createObjectURL(event.target.files[0]);
-    });
-
     $("#verifyMail").on('select keyup change select',function(){
         $.ajax({
                 url: Routing.generate('verify_mail'),
@@ -51,30 +45,7 @@ $(document).ready(function () {
             });
     });
 
-    $('.date').datepicker({
-        format: "yyyy-mm-dd",
-        startView: 2,
-        autoclose: true
-    });
-
-    $('.addSkill').click(function () {
-        var skill = {
-            id: "",
-            name: $(".inputSkill").val()
-        };
-        allSkills.push(skill);
-        $(".inputSkill").val("");
-        $(".skillsTable tbody").append("<tr class='skills'><td><input type='hidden' value='" + skill.name + "' name='skills[" + count + "][name]' class='skillName' />" + skill.name + "</td><td><input type='hidden' value='" + skill.id + "' name='skills[" + count + "][id]' class='skillId' /><button type='button' class='remove btn btn-default btn-xs'><i class='fa fa-minus'></i></button></td></tr>");
-        count = count + 1;
-    });
-
-    $("tbody").on('click', ".remove", function () {
-        var index = $(this).closest("tr").index()
-        allSkills.splice(index, 1);
-        $(this).closest('tr').remove();
-    });
-
-    $(".inputSkill").autocomplete({
+ /* $(".inputSkill").autocomplete({
         source: function( request, response ) {
             $.ajax({
                 url: Routing.generate('verify_skill'),
@@ -101,5 +72,5 @@ $(document).ready(function () {
             event.preventDefault();
             $(this).val(ui.item.label);
         }
-    });
+    });*/
 });
