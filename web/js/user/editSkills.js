f0f723ce51647972118dac75a8102c62243f6311
zairavelasquezm <zairavelasquezm@gmail.com>
2015-12-10 23:56:51 -0800

    correccion skill js

--- web/js/user/editSkills.js
+++ web/js/user/editSkills.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
     var allSkills = [];
     var count = 0;
 
+    $('.btn-file :file').on('change', function (event) {
+        var input = $(this).parents('.input-group').find(':text');
+        var output = document.getElementById('output');
+        output.src = URL.createObjectURL(event.target.files[0]);
+    });
+
+    $('.date').datepicker({
+        format: "yyyy-mm-dd",
+        startView: 2,
+        autoclose: true
+    });
 
     $(".skills").each(function(index) {
         allSkills[index] = {};
@@ -9,9 +20,7 @@ $(document).ready(function() {
         allSkills[index].id = $(this).find(".skillId").val();
     });
 
-    console.log(allSkills);
-
-    function remove(array, property, value){
+    function removeSkill(array, property, value){
         var i, cur;
         for(i= array.length - 1; i >= 0; i--){
             cur = array[i];
@@ -21,32 +30,30 @@ $(document).ready(function() {
         }
     }
 
-    $('.btn-file :file').on('change', function (event) {
-        var input = $(this).parents('.input-group').find(':text');
-        var output = document.getElementById('output');
-        output.src = URL.createObjectURL(event.target.files[0]);
+    $('#insertSkill').keydown(function(event){
+        if(event.keyCode == 13){
+            event.preventDefault();
+            var skill = {
+                id: "",
+                name: $(".inputSkill").val()
+            };
+            allSkills.push(skill);
+            $(".inputSkill").val("");
+            $(".skillsTable tbody").append("<tr class='skills'><td><input type='hidden' value='" + skill.name + "' name='skills[" + count + "][name]' class='skillName' />" + skill.name + "</td><td><input type='hidden' value='" + skill.id + "' name='skills[" + count + "][id]' class='skillId' /><button type='button' class='remove btn btn-default btn-xs'><i class='fa fa-minus'></i></button></td></tr>");
+            count = count + 1;
+        }
     });
 
-    $('.addSkill').click(function () {
-        var skill = {
-            id: "",
-            name: $(".inputSkill").val()
-        };
-        allSkills.push(skill);
-        $(".inputSkill").val("");
-        $(".skillsTable tbody").append("<tr class='skills'><td><input type='hidden' value='" + skill.name + "' name='skills[" + count + "][name]' class='skillName' />" + skill.name + "</td><td><input type='hidden' value='" + skill.id + "' name='skills[" + count + "][id]' class='skillId' /><button type='button' class='remove btn btn-default btn-xs'><i class='fa fa-minus'></i></button></td></tr>");
-        count = count + 1;
-        console.log(allSkills);
-    });
 
     $("tbody").on('click', ".remove", function () {
-        var index = $(this).closest("tr").index()
+        var index = $(this).closest("tr").index();
         allSkills.splice(index, 1);
         $(this).closest('tr').remove();
         console.log(allSkills);
     });
 
-    $(".inputSkill").autocomplete({
+
+    $("#autocompleteSkill").autocomplete({
         source: function( request, response ) {
             $.ajax({
                 url: Routing.generate('verify_skill'),
@@ -54,7 +61,7 @@ $(document).ready(function() {
                 data: {term: request.term},
                 success: function(data) {
                     $.each((allSkills), function() {
-                        remove(data, "value", this.id);
+                        removeSkill(data, "value", this.id);
                     });
                     response(data);
                 }
@@ -67,20 +74,20 @@ $(document).ready(function() {
             $(".skillsTable tbody").append("<tr class='skills'><td><input type='hidden' value='" + ui.item.label + "' name='skills[" + count + "][name]' class='skillName' />" + ui.item.label + "</td><td><input type='hidden' value='" + ui.item.value + "' name='skills[" + count + "][id]' class='skillId' /><button type='button' class='remove btn btn-default btn-xs'><i class='fa fa-minus'></i></button></td></tr>");
             count = count + 1;
             $(this).val("");
-            return false;
+            var originalEvent = event;
+            while (originalEvent) {
+                if (originalEvent.keyCode == 13)
+                    originalEvent.stopPropagation();
+
+                if (originalEvent == event.originalEvent)
+                    break;
+
+                originalEvent = event.originalEvent;
+            }
         },
         focus: function( event, ui ) {
             event.preventDefault();
             $(this).val(ui.item.label);
         }
     });
-
-    $('.date').datepicker({
-        format: "yyyy-mm-dd",
-        startView: 2,
-        autoclose: true
-    });
 });
-
-
-
